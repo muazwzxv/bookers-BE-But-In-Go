219bcedfdbd559000c94e980b33280d4c0f845ad
Muaz Wazir <muazwazir@gmail.com>
2021-04-17 00:27:18 +0800

    go mod && main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "You are at the endpoint",
+		})
+	})
+
+	err := app.Listen(":8000")
+
+	if err != nil {
+		panic(err)
+	}
+}
